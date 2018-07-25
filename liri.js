35493b6a06ba86f17e44f3245d3c67202cc7090d
unknown <monmaramba@yahoo.com>
2018-07-24 20:00:11 -0700

    getting started

--- liri.js
+++ liri.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 //code to import the keys.js file and store to variable
+var keys = require("./keys.js");
+
 
 var spotify = new Spotify(keys.spotify);
   var client = new Twitter(keys.twitter);
